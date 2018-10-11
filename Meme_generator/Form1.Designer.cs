a51846d83b3efeb050f54daddf8c74cba9a3d7ab
CaramelYo_CGVSL_PC <alltime326@gmail.com>
2018-10-11 21:53:45 +0800

    first commit

---
+++ Meme_generator/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Meme_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.image_tp = new System.Windows.Forms.TabPage();
+            this.image_rb_3 = new System.Windows.Forms.RadioButton();
+            this.image_rb_2 = new System.Windows.Forms.RadioButton();
+            this.image_rb_1 = new System.Windows.Forms.RadioButton();
+            this.image_rb_0 = new System.Windows.Forms.RadioButton();
+            this.font_style_tp = new System.Windows.Forms.TabPage();
+            this.font_size_minus_btn = new System.Windows.Forms.Button();
+            this.font_size_plus_btn = new System.Windows.Forms.Button();
+            this.font_style_rb_0 = new System.Windows.Forms.RadioButton();
+            this.font_style_rb_3 = new System.Windows.Forms.RadioButton();
+            this.font_style_rb_1 = new System.Windows.Forms.RadioButton();
+            this.font_style_rb_2 = new System.Windows.Forms.RadioButton();
+            this.sentence_tp = new System.Windows.Forms.TabPage();
+            this.sentence_rb_3 = new System.Windows.Forms.RadioButton();
+            this.sentence_rb_2 = new System.Windows.Forms.RadioButton();
+            this.sentence_rb_1 = new System.Windows.Forms.RadioButton();
+            this.sentence_rb_0 = new System.Windows.Forms.RadioButton();
+            this.random_btn = new System.Windows.Forms.Button();
+            this.sentence_label = new System.Windows.Forms.Label();
+            this.picturebox_0 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.image_tp.SuspendLayout();
+            this.font_style_tp.SuspendLayout();
+            this.sentence_tp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox_0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.image_tp);
+            this.tabControl1.Controls.Add(this.font_style_tp);
+            this.tabControl1.Controls.Add(this.sentence_tp);
+            this.tabControl1.Location = new System.Drawing.Point(113, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(412, 152);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // image_tp
+            // 
+            this.image_tp.Controls.Add(this.image_rb_3);
+            this.image_tp.Controls.Add(this.image_rb_2);
+            this.image_tp.Controls.Add(this.image_rb_1);
+            this.image_tp.Controls.Add(this.image_rb_0);
+            this.image_tp.Location = new System.Drawing.Point(4, 22);
+            this.image_tp.Name = "image_tp";
+            this.image_tp.Padding = new System.Windows.Forms.Padding(3);
+            this.image_tp.Size = new System.Drawing.Size(404, 126);
+            this.image_tp.TabIndex = 0;
+            this.image_tp.Text = "image";
+            this.image_tp.UseVisualStyleBackColor = true;
+            // 
+            // image_rb_3
+            // 
+            this.image_rb_3.AutoSize = true;
+            this.image_rb_3.Location = new System.Drawing.Point(241, 67);
+            this.image_rb_3.Name = "image_rb_3";
+            this.image_rb_3.Size = new System.Drawing.Size(46, 16);
+            this.image_rb_3.TabIndex = 3;
+            this.image_rb_3.TabStop = true;
+            this.image_rb_3.Text = "doge";
+            this.image_rb_3.UseVisualStyleBackColor = true;
+            // 
+            // image_rb_2
+            // 
+            this.image_rb_2.AutoSize = true;
+            this.image_rb_2.Location = new System.Drawing.Point(69, 67);
+            this.image_rb_2.Name = "image_rb_2";
+            this.image_rb_2.Size = new System.Drawing.Size(78, 16);
+            this.image_rb_2.TabIndex = 2;
+            this.image_rb_2.TabStop = true;
+            this.image_rb_2.Text = "evil_patrick";
+            this.image_rb_2.UseVisualStyleBackColor = true;
+            // 
+            // image_rb_1
+            // 
+            this.image_rb_1.AutoSize = true;
+            this.image_rb_1.Location = new System.Drawing.Point(241, 19);
+            this.image_rb_1.Name = "image_rb_1";
+            this.image_rb_1.Size = new System.Drawing.Size(95, 16);
+            this.image_rb_1.TabIndex = 1;
+            this.image_rb_1.TabStop = true;
+            this.image_rb_1.Text = "excuse_me_wtf";
+            this.image_rb_1.UseVisualStyleBackColor = true;
+            // 
+            // image_rb_0
+            // 
+            this.image_rb_0.AutoSize = true;
+            this.image_rb_0.Location = new System.Drawing.Point(69, 19);
+            this.image_rb_0.Name = "image_rb_0";
+            this.image_rb_0.Size = new System.Drawing.Size(69, 16);
+            this.image_rb_0.TabIndex = 0;
+            this.image_rb_0.TabStop = true;
+            this.image_rb_0.Text = "just_do_it";
+            this.image_rb_0.UseVisualStyleBackColor = true;
+            // 
+            // font_style_tp
+            // 
+            this.font_style_tp.Controls.Add(this.font_size_minus_btn);
+            this.font_style_tp.Controls.Add(this.font_size_plus_btn);
+            this.font_style_tp.Controls.Add(this.font_style_rb_0);
+            this.font_style_tp.Controls.Add(this.font_style_rb_1);
+            this.font_style_tp.Controls.Add(this.font_style_rb_2);
+            this.font_style_tp.Controls.Add(this.font_style_rb_3);
+            this.font_style_tp.Location = new System.Drawing.Point(4, 22);
+            this.font_style_tp.Name = "font_style_tp";
+            this.font_style_tp.Padding = new System.Windows.Forms.Padding(3);
+            this.font_style_tp.Size = new System.Drawing.Size(404, 126);
+            this.font_style_tp.TabIndex = 1;
+            this.font_style_tp.Text = "font style";
+            this.font_style_tp.UseVisualStyleBackColor = true;
+            // 
+            // font_size_minus_btn
+            // 
+            this.font_size_minus_btn.Location = new System.Drawing.Point(254, 67);
+            this.font_size_minus_btn.Name = "font_size_minus_btn";
+            this.font_size_minus_btn.Size = new System.Drawing.Size(75, 23);
+            this.font_size_minus_btn.TabIndex = 4;
+            this.font_size_minus_btn.Text = "font size -";
+            this.font_size_minus_btn.UseVisualStyleBackColor = true;
+            this.font_size_minus_btn.Click += new System.EventHandler(this.font_size_minus_btn_Click);
+            // 
+            // font_size_plus_btn
+            // 
+            this.font_size_plus_btn.Location = new System.Drawing.Point(254, 20);
+            this.font_size_plus_btn.Name = "font_size_plus_btn";
+            this.font_size_plus_btn.Size = new System.Drawing.Size(75, 23);
+            this.font_size_plus_btn.TabIndex = 3;
+            this.font_size_plus_btn.Text = "font size +";
+            this.font_size_plus_btn.UseVisualStyleBackColor = true;
+            this.font_size_plus_btn.Click += new System.EventHandler(this.font_size_plus_btn_Click);
+            // 
+            // font_style_rb_0
+            // 
+            this.font_style_rb_0.AutoSize = true;
+            this.font_style_rb_0.Location = new System.Drawing.Point(22, 20);
+            this.font_style_rb_0.Name = "font_style_rb_0";
+            this.font_style_rb_0.Size = new System.Drawing.Size(60, 16);
+            this.font_style_rb_0.TabIndex = 3;
+            this.font_style_rb_0.TabStop = true;
+            this.font_style_rb_0.Text = "Regular";
+            this.font_style_rb_0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.font_style_rb_0.UseVisualStyleBackColor = true;
+            // 
+            // font_style_rb_3
+            // 
+            this.font_style_rb_3.AutoSize = true;
+            this.font_style_rb_3.Location = new System.Drawing.Point(144, 74);
+            this.font_style_rb_3.Name = "font_style_rb_3";
+            this.font_style_rb_3.Size = new System.Drawing.Size(69, 16);
+            this.font_style_rb_3.TabIndex = 2;
+            this.font_style_rb_3.TabStop = true;
+            this.font_style_rb_3.Text = "Underline";
+            this.font_style_rb_3.UseVisualStyleBackColor = true;
+            // 
+            // font_style_rb_1
+            // 
+            this.font_style_rb_1.AutoSize = true;
+            this.font_style_rb_1.Location = new System.Drawing.Point(144, 23);
+            this.font_style_rb_1.Name = "font_style_rb_1";
+            this.font_style_rb_1.Size = new System.Drawing.Size(46, 16);
+            this.font_style_rb_1.TabIndex = 1;
+            this.font_style_rb_1.TabStop = true;
+            this.font_style_rb_1.Text = "Bold";
+            this.font_style_rb_1.UseVisualStyleBackColor = true;
+            // 
+            // font_style_rb_2
+            // 
+            this.font_style_rb_2.AutoSize = true;
+            this.font_style_rb_2.Location = new System.Drawing.Point(22, 70);
+            this.font_style_rb_2.Name = "font_style_rb_2";
+            this.font_style_rb_2.Size = new System.Drawing.Size(46, 16);
+            this.font_style_rb_2.TabIndex = 0;
+            this.font_style_rb_2.TabStop = true;
+            this.font_style_rb_2.Text = "Italic";
+            this.font_style_rb_2.UseVisualStyleBackColor = true;
+            // 
+            // sentence_tp
+            // 
+            this.sentence_tp.Controls.Add(this.sentence_rb_3);
+            this.sentence_tp.Controls.Add(this.sentence_rb_2);
+            this.sentence_tp.Controls.Add(this.sentence_rb_1);
+            this.sentence_tp.Controls.Add(this.sentence_rb_0);
+            this.sentence_tp.Location = new System.Drawing.Point(4, 22);
+            this.sentence_tp.Name = "sentence_tp";
+            this.sentence_tp.Size = new System.Drawing.Size(404, 126);
+            this.sentence_tp.TabIndex = 2;
+            this.sentence_tp.Text = "sentence";
+            this.sentence_tp.UseVisualStyleBackColor = true;
+            // 
+            // sentence_rb_3
+            // 
+            this.sentence_rb_3.AutoSize = true;
+            this.sentence_rb_3.Location = new System.Drawing.Point(14, 78);
+            this.sentence_rb_3.Name = "sentence_rb_3";
+            this.sentence_rb_3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.sentence_rb_3.Size = new System.Drawing.Size(220, 16);
+            this.sentence_rb_3.TabIndex = 3;
+            this.sentence_rb_3.TabStop = true;
+            this.sentence_rb_3.Text = "Professor：This is a group project. You：";
+            this.sentence_rb_3.UseVisualStyleBackColor = true;
+            // 
+            // sentence_rb_2
+            // 
+            this.sentence_rb_2.AutoSize = true;
+            this.sentence_rb_2.Location = new System.Drawing.Point(14, 56);
+            this.sentence_rb_2.Name = "sentence_rb_2";
+            this.sentence_rb_2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.sentence_rb_2.Size = new System.Drawing.Size(197, 16);
+            this.sentence_rb_2.TabIndex = 2;
+            this.sentence_rb_2.TabStop = true;
+            this.sentence_rb_2.Text = "The moment she peeks at your phone";
+            this.sentence_rb_2.UseVisualStyleBackColor = true;
+            // 
+            // sentence_rb_1
+            // 
+            this.sentence_rb_1.AutoSize = true;
+            this.sentence_rb_1.Location = new System.Drawing.Point(14, 34);
+            this.sentence_rb_1.Name = "sentence_rb_1";
+            this.sentence_rb_1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.sentence_rb_1.Size = new System.Drawing.Size(169, 16);
+            this.sentence_rb_1.TabIndex = 1;
+            this.sentence_rb_1.TabStop = true;
+            this.sentence_rb_1.Text = "Mom: how\'s midterm？ You：";
+            this.sentence_rb_1.UseVisualStyleBackColor = true;
+            // 
+            // sentence_rb_0
+            // 
+            this.sentence_rb_0.AutoSize = true;
+            this.sentence_rb_0.Location = new System.Drawing.Point(14, 12);
+            this.sentence_rb_0.Name = "sentence_rb_0";
+            this.sentence_rb_0.Size = new System.Drawing.Size(112, 16);
+            this.sentence_rb_0.TabIndex = 0;
+            this.sentence_rb_0.TabStop = true;
+            this.sentence_rb_0.Text = "Want Some Weed?";
+            this.sentence_rb_0.UseVisualStyleBackColor = true;
+            // 
+            // random_btn
+            // 
+            this.random_btn.Location = new System.Drawing.Point(446, 196);
+            this.random_btn.Name = "random_btn";
+            this.random_btn.Size = new System.Drawing.Size(75, 23);
+            this.random_btn.TabIndex = 1;
+            this.random_btn.Text = "random select";
+            this.random_btn.UseVisualStyleBackColor = true;
+            this.random_btn.Click += new System.EventHandler(this.random_btn_Click);
+            // 
+            // sentence_label
+            // 
+            this.sentence_label.AutoSize = true;
+            this.sentence_label.Location = new System.Drawing.Point(214, 238);
+            this.sentence_label.Name = "sentence_label";
+            this.sentence_label.Size = new System.Drawing.Size(72, 12);
+            this.sentence_label.TabIndex = 2;
+            this.sentence_label.Text = "sentence_label";
+            // 
+            // picturebox_0
+            // 
+            this.picturebox_0.Location = new System.Drawing.Point(121, 272);
+            this.picturebox_0.Name = "picturebox_0";
+            this.picturebox_0.Size = new System.Drawing.Size(404, 266);
+            this.picturebox_0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picturebox_0.TabIndex = 3;
+            this.picturebox_0.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 546);
+            this.Controls.Add(this.picturebox_0);
+            this.Controls.Add(this.sentence_label);
+            this.Controls.Add(this.random_btn);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.image_tp.ResumeLayout(false);
+            this.image_tp.PerformLayout();
+            this.font_style_tp.ResumeLayout(false);
+            this.font_style_tp.PerformLayout();
+            this.sentence_tp.ResumeLayout(false);
+            this.sentence_tp.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox_0)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage image_tp;
+        private System.Windows.Forms.RadioButton image_rb_3;
+        private System.Windows.Forms.RadioButton image_rb_2;
+        private System.Windows.Forms.RadioButton image_rb_1;
+        private System.Windows.Forms.RadioButton image_rb_0;
+        private System.Windows.Forms.TabPage font_style_tp;
+        private System.Windows.Forms.TabPage sentence_tp;
+        private System.Windows.Forms.RadioButton font_style_rb_0;
+        private System.Windows.Forms.RadioButton font_style_rb_3;
+        private System.Windows.Forms.RadioButton font_style_rb_1;
+        private System.Windows.Forms.RadioButton font_style_rb_2;
+        private System.Windows.Forms.Button random_btn;
+        private System.Windows.Forms.Label sentence_label;
+        private System.Windows.Forms.Button font_size_minus_btn;
+        private System.Windows.Forms.Button font_size_plus_btn;
+        private System.Windows.Forms.RadioButton sentence_rb_1;
+        private System.Windows.Forms.RadioButton sentence_rb_0;
+        private System.Windows.Forms.RadioButton sentence_rb_2;
+        private System.Windows.Forms.RadioButton sentence_rb_3;
+        private System.Windows.Forms.PictureBox picturebox_0;
+    }
+}
+
---
+++ Meme_generator/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meme_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            total_rbs = new List<List<RadioButton>>();
+
+            foreach (TabPage tp in tabControl1.TabPages)
+            {
+                List<RadioButton> rbs = new List<RadioButton>();
+
+                foreach (Control c in tp.Controls)
+                    if (c.GetType() == typeof(RadioButton))
+                        rbs.Add((RadioButton)c);
+                
+                total_rbs.Add(rbs);
+            }
+
+            r = new Random();
+
+            img_dir = "imgs/";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // check the first radiobutton in every tabpage
+            foreach (List<RadioButton> rbs in total_rbs)
+                foreach (RadioButton rb in rbs)
+                    rb.Checked = false;
+            
+            foreach (List<RadioButton> rbs in total_rbs)
+                rbs[0].Checked = true;
+        }
+
+        private void generate()
+        {
+            // 3 tabpages
+            int[] selections = new int[3];
+
+            // record which radiobutton is checked
+            for (int i = 0; i < 3; ++i)
+                for (int j = 0; j < 4; ++j)
+                    if (total_rbs[i][j].Checked)
+                    {
+                        selections[i] = j;
+                        break;
+                    }
+            
+            picturebox_0.Image = Image.FromFile(img_dir + total_rbs[0][selections[0]].Text + ".png");
+            
+            FontStyle new_fs;
+
+            switch (selections[1])
+            {
+                case 0:
+                    new_fs = FontStyle.Regular;
+                    break;
+                case 1:
+                    new_fs = FontStyle.Bold;
+                    break;
+                case 2:
+                    new_fs = FontStyle.Italic;
+                    break;
+                case 3:
+                    new_fs = FontStyle.Underline;
+                    break;
+                default:
+                    new_fs = FontStyle.Regular;
+                    break;
+            }
+
+            sentence_label.Font = new Font(
+                        sentence_label.Font.Name,
+                        sentence_label.Font.Size,
+                        new_fs
+                    );
+            
+            sentence_label.Text = total_rbs[2][selections[2]].Text;
+        }
+
+        private void random_btn_Click(object sender, EventArgs e)
+        {
+            foreach (List<RadioButton> rbs in total_rbs)
+            {
+                int idx = r.Next(rbs.Count);
+                rbs[idx].Checked = true;
+            }
+
+            generate();
+        }
+
+        private void change_font_size(float size)
+        {
+            sentence_label.Font = new Font(
+                sentence_label.Font.Name,
+                size,
+                sentence_label.Font.Style
+            );
+
+            generate();
+        }
+
+        private void font_size_plus_btn_Click(object sender, EventArgs e)
+        {
+            change_font_size(sentence_label.Font.Size + 1);
+        }
+
+        private void font_size_minus_btn_Click(object sender, EventArgs e)
+        {
+            change_font_size(sentence_label.Font.Size - 1);
+        }
+
+        List<List<RadioButton>> total_rbs;
+        Random r;
+
+        string img_dir;
+    }
+}
